4aa35df4839e1c44ce3c32cea2cd1e397c74aaf3
gouse131 <118803768+gouse131@users.noreply.github.com>
2023-01-10 19:20:47 +0530

    Changes made

--- Stanford Assignment.java
+++ Stanford Assignment.java
@@ -4,28 +4,28 @@ public class Assignment{
     
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int Sequenceofnumbers = scan.nextInt();
-        int array[]=new int[Sequenceofnumbers];
+        int sequenceOfNumbers = scan.nextInt();
+        int array[] = new int[sequenceOfNumbers];
  
-        for(int i=0;i<Sequenceofnumbers;i++){
+        for(int i = 0;i<sequenceOfNumbers;i++){
             array[i] = scan.nextInt();
         }
         int evenCount = 0;
-        int oddEvent = 0;
+        int oddCount = 0;
  
-        for(int i=0;i<Sequenceofnumbers;i++) {
+        for(int i = 0;i<sequenceOfNumbers;i++) {
             if( array[i] % 2 == 0 ){
                 evenCount++;
             } else {
-                oddEvent++;
+                oddCount++;
             }
         }
-        int oddRemainder=c2%4;
-        int evenRemainder=c1%2;             
-        if(oddRemainder ==3|| oddRemainder==0||(evenRemainder==1 && evenRemainder==1)){
+        int oddRemainder = oddCount%4;
+        int evenRemainder = evenCount%2;             
+        if(oddRemainder == 3 || oddRemainder == 0|| (evenRemainder == 1 && evenRemainder == 1)){
             System.out.println("Alice Win");
             }else{
             System.out.println("Bob Win");
-       }
+        }
     }
 }
